b7e3c0841681558c99bd68db1ee4a3b168defd49
mbogdos96 <michael_bogdos@hotmail.com>
2025-07-25 16:55:01 -0400

    Finished sims for sd n relationship and planned out question 4

--- question_1.R
+++ question_1.R
@@ -37,3 +37,63 @@ sample_plot <- ggplot(data = sample_df) +
   theme_classic()
 
 print(sample_plot)
+
+#===============
+# Answer portion
+#===============
+# Simulate the answers
+# Function for simulations
+sd_n_sims_fn <- function(n_seq){
+  # Generate a normal sample
+  norm_sample <- rnorm(1000000, 
+                        mean = 0, 
+                        sd = 100)
+  
+  # Create an empty data frame
+  df <- data.frame(n = numeric(), 
+                   sd = numeric())
+  
+  # Loop through each sample size (n)
+  for (n in n_seq){
+    # Create 100 samples for each sample size
+    for (i in 1:100){
+      # Generate a sample of size n from 
+      # the "normal" variable
+      sample_data <- sample(norm_sample, 
+                            n)
+      
+      # Calculate the standard 
+      # deviation of the sample
+      sample_sd <- sd(sample_data)
+      
+      # Append the sample size (n) 
+      # and sample standard deviation (sd) 
+      # to the data frame
+      df <- rbind(df, data.frame(n = n, 
+                                 sd = sample_sd))
+    }
+  }
+  
+  return(df)
+}
+
+# Simulate for n from 3 to 30
+sd_n_three_thirty <- sd_n_sims_fn(seq(3,30,by = 1)) %>%
+  group_by(n) %>% 
+  summarize(average.sd = mean(sd),
+            upr_sd = average.sd + sd(sd),
+            lwr_sd = average.sd - sd(sd))
+
+# Plot the relationship
+sd_n_plot_sims <- ggplot(data = sd_n_three_thirty,
+                         aes(x = n, y = average.sd)) +
+  geom_hline(yintercept = 100) +
+  geom_point() +
+  geom_errorbar(aes(ymin = lwr_sd, ymax = upr_sd),
+                width = 0.01,
+                alpha = 0.8) +
+  labs(x = "Sample size (n)",
+       y = "Average standard deviation (100 sims)") +
+  theme_classic()
+
+print(sd_n_plot_sims)
---
+++ question_4.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(ggplot2)
+library(ggbeeswarm)
+
+# Four graphs
+# Graph A, 5 treatment groups with bar graphs and SE
+
+# Graph B, 5 treatment groups with bar graphs and sd
+
+# Graph C, box plot
+
+# Graph D, beeswarm plot with sd error bar and another for SE
